73a9478c403d73f176bd7cc6675373cd07a15564
MJB222398 <maxbradley64@gmail.com>
2025-03-12 17:41:19 +0000

    bug: Revert accidental formatting changes (#153)

--- src/main/java/hudson/plugins/junitattachments/AttachmentTestAction.java
+++ src/main/java/hudson/plugins/junitattachments/AttachmentTestAction.java
@@ -5,63 +5,61 @@ import hudson.Util;
 import hudson.model.DirectoryBrowserSupport;
 import hudson.tasks.junit.TestAction;
 import hudson.tasks.test.TestObject;
-
 import java.util.List;
-
 import jenkins.model.Jenkins;
 
 public class AttachmentTestAction extends TestAction {
 
-    private final FilePath storage;
-    private final List<String> attachments;
-    private final TestObject testObject;
+	private final FilePath storage;
+	private final List<String> attachments;
+	private final TestObject testObject;
 
-    public AttachmentTestAction(TestObject testObject, FilePath storage, List<String> attachments) {
-        this.storage = storage;
-        this.testObject = testObject;
-        this.attachments = attachments;
-    }
+	public AttachmentTestAction(TestObject testObject, FilePath storage, List<String> attachments) {
+		this.storage = storage;
+		this.testObject = testObject;
+		this.attachments = attachments;
+	}
 
-    public String getDisplayName() {
-        return "Attachments";
-    }
+	public String getDisplayName() {
+		return "Attachments";
+	}
 
-    public String getIconFileName() {
-        return "package.gif";
-    }
+	public String getIconFileName() {
+		return "package.gif";
+	}
 
-    public String getUrlName() {
-        return "attachments";
-    }
+	public String getUrlName() {
+		return "attachments";
+	}
 
-    public DirectoryBrowserSupport doDynamic() {
-        return new DirectoryBrowserSupport(this, storage, "Attachments", "package.gif", true);
-    }
+	public DirectoryBrowserSupport doDynamic() {
+		return new DirectoryBrowserSupport(this, storage, "Attachments", "package.gif", true);
+	}
 
-    @Override
-    public String annotate(String text) {
+	@Override
+	public String annotate(String text) {
+		String url = Jenkins.get().getRootUrl() + testObject.getUrl() + "/attachments/";
+		for (String attachment : attachments) {
+			text = text.replace(attachment, "<a href=\"" + url + attachment
+					+ "\">" + attachment + "</a>");
+		}
+		return text;
+	}
 
-        String url = Jenkins.get().getRootUrl() + testObject.getUrl() + "/attachments/";
-        for (String attachment : attachments) {
-            text = text.replace(attachment, "<a href=\"" + url + attachment
-                    + "\">" + attachment + "</a>");
-        }
-        return text;
-    }
+	public List<String> getAttachments() {
+		return attachments;
+	}
 
-    public List<String> getAttachments() {
-        return attachments;
-    }
+	public TestObject getTestObject() {
+		return testObject;
+	}
 
-    public TestObject getTestObject() {
-        return testObject;
-    }
+	public static boolean isImageFile(String filename) {
+		return filename.matches("(?i).+\\.(gif|jpe?g|png)$");
+	}
 
-    public static boolean isImageFile(String filename) {
-        return filename.matches("(?i).+\\.(gif|jpe?g|png)$");
-    }
+	public static String getUrl(String filename) {
+		return "attachments/" + Util.rawEncode(filename);
+	}
 
-    public static String getUrl(String filename) {
-        return "attachments/" + Util.rawEncode(filename);
-    }
 }
