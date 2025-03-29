6265935738514c6b8f6532d780ff038f4161c5c2
Max Bradley <maxbradley64@gmail.com>
2025-03-29 21:49:03 +0000

    feat: Add option to hide list of attachments in standard output (#163)

--- src/main/java/hudson/plugins/junitattachments/TestCaseAttachmentTestAction.java
+++ src/main/java/hudson/plugins/junitattachments/TestCaseAttachmentTestAction.java
@@ -6,15 +6,21 @@ import hudson.tasks.junit.CaseResult;
 import jenkins.model.Jenkins;
 
 import java.util.List;
+import java.util.regex.Pattern;
 
 public class TestCaseAttachmentTestAction extends AttachmentTestAction {
 
-    private final List<String> attachments;
+    private static final Pattern ATTACHMENT_PATTERN = Pattern.compile("\\[\\[ATTACHMENT\\|.+]]");
 
-    public TestCaseAttachmentTestAction(CaseResult caseResult, FilePath storage, List<String> attachments) {
+    private final List<String> attachments;
+    private final boolean showAttachmentsInStdOut;
+
+    public TestCaseAttachmentTestAction(
+            CaseResult caseResult, FilePath storage, List<String> attachments, boolean showAttachmentsInStdOut) {
         super(caseResult, storage);
 
         this.attachments = attachments;
+        this.showAttachmentsInStdOut = showAttachmentsInStdOut;
     }
 
     public List<String> getAttachments() {
@@ -23,11 +29,17 @@ public class TestCaseAttachmentTestAction extends AttachmentTestAction {
 
     @Override
     public String annotate(String text) {
+
+        if (!showAttachmentsInStdOut) {
+            text = ATTACHMENT_PATTERN.matcher(text).replaceAll("").stripTrailing();
+        }
+
         String url = Jenkins.get().getRootUrl() + testObject.getUrl() + "/attachments/";
         for (String attachment : attachments) {
             text = text.replace(attachment, "<a href=\"" + url + attachment
                     + "\">" + attachment + "</a>");
         }
+
         return text;
     }
 
